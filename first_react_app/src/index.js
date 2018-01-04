3af122e99bbd23e82d0c23687e10b41d3a7c35ab
Yu Yang <yu.yang@cittacorp.com>
2018-01-04 12:55:24 -0500

    first react app

---
+++ first_react_app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ClickCounter from './ClickCounter';
+import './index.css';
+
+ReactDOM.render( <ClickCounter /> , document.getElementById('root'));
